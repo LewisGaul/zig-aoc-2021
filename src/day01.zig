30dc77b6e44f3c1f6f71424659a94e2068545e76
Lewis Gaul <>
2021-12-01 18:43:34 +0000

    Add solution for 1b, plus a couple more tests

--- src/day01.zig
+++ src/day01.zig
@@ -17,7 +17,7 @@ const LineIter = struct {
 
     pub fn next(self: *Self) ParseIntError!?u32 {
         if (self._iter.next()) |line| {
-            const trimmed = std.mem.trim(u8, line, " \n\r");
+            const trimmed = std.mem.trim(u8, line, " \t\r");
             if (trimmed.len == 0) return try self.next();
             return try std.fmt.parseUnsigned(u32, trimmed, 10);
         } else {
@@ -26,7 +26,7 @@ const LineIter = struct {
     }
 };
 
-fn mainFromData(input: Str) !u32 {
+fn part1(input: Str) !u32 {
     var answer: u32 = 0;
     var iter = LineIter.init(input);
     var prev_num: ?u32 = try iter.next();
@@ -37,21 +37,88 @@ fn mainFromData(input: Str) !u32 {
     return answer;
 }
 
-pub fn main() !void {
-    print("Answer: {d}\n", .{try mainFromData(data)});
+fn part2(input: Str) !u32 {
+    var answer: u32 = 0;
+    var iter = LineIter.init(input);
+    var num1: ?u32 = try iter.next();
+    var num2: ?u32 = try iter.next();
+    var num3: ?u32 = try iter.next();
+    while (try iter.next()) |next_num| {
+        if (next_num > num1.?) answer += 1;
+        num1 = num2;
+        num2 = num3;
+        num3 = next_num;
+    }
+    return answer;
 }
 
-test {
+pub fn main() !void {
+    print("Part 1\n-------\n", .{});
+    print("Answer: {d}\n", .{try part1(data)});
+    print("Part 2\n-------\n", .{});
+    print("Answer: {d}\n", .{try part2(data)});
+}
+
+test "1a" {
+    try std.testing.expectEqual(@as(u32, 0), try part1(""));
+    try std.testing.expectEqual(@as(u32, 0), try part1("100"));
+    try std.testing.expectEqual(@as(u32, 0), try part1("100\n99"));
+    try std.testing.expectEqual(@as(u32, 0), try part1("100\n100"));
+    try std.testing.expectEqual(@as(u32, 1), try part1("100\n101"));
+    try std.testing.expectEqual(@as(u32, 2), try part1("100\n101\n102\n"));
+    try std.testing.expectEqual(@as(u32, 1), try part1("  100  \n  101  "));
+    try std.testing.expectError(error.InvalidCharacter, part1("text"));
+
+    {
+        const input =
+            \\199
+            \\200
+            \\208
+            \\210
+            \\200
+            \\207
+            \\240
+            \\269
+            \\260
+            \\263
+        ;
+        try std.testing.expectEqual(@as(u32, 7), try part1(input));
+    }
+
+    {
+        const input =
+            \\123
+            \\124
+            \\  125
+            \\100
+            \\
+            \\100
+            \\20
+            \\21
+            \\
+        ;
+        try std.testing.expectEqual(@as(u32, 3), try part1(input));
+    }
+}
+
+test "1b" {
+    try std.testing.expectEqual(@as(u32, 0), try part2(""));
+    try std.testing.expectEqual(@as(u32, 0), try part2("100"));
+    try std.testing.expectEqual(@as(u32, 0), try part2("100\n101\n102"));
+    try std.testing.expectEqual(@as(u32, 1), try part2("100\n101\n102\n103"));
+    try std.testing.expectError(error.InvalidCharacter, part2("text"));
+
     const input =
-        \\123
-        \\124
-        \\  125
-        \\100
-        \\
-        \\100
-        \\20
-        \\21
-        \\
+        \\199
+        \\200
+        \\208
+        \\210
+        \\200
+        \\207
+        \\240
+        \\269
+        \\260
+        \\263
     ;
-    try std.testing.expectEqual(@as(u32, 3), try mainFromData(input));
+    try std.testing.expectEqual(@as(u32, 5), try part2(input));
 }
