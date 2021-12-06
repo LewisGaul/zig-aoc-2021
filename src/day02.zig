94c436a3d824bb2044800d4b1c0d1c888f00c795
Lewis Gaul <>
2021-12-06 14:46:39 +0000

    Add solution to 2b

--- src/day02.zig
+++ src/day02.zig
@@ -8,7 +8,7 @@ const data = @embedFile("../data/day02.txt");
 
 const Str = []const u8;
 
-fn readLineNum(input: Str, idx: *usize) !u32 {
+fn readLineNum(input: Str, idx: *usize) !u8 {
     var start_idx: ?usize = null;
     while (idx.* < input.len) : (idx.* += 1) {
         switch (input[idx.*]) {
@@ -20,7 +20,7 @@ fn readLineNum(input: Str, idx: *usize) !u32 {
         }
     }
     assert(start_idx != null);
-    return std.fmt.parseUnsigned(u32, input[start_idx.?..idx.*], 10);
+    return std.fmt.parseUnsigned(u8, input[start_idx.?..idx.*], 10);
 }
 
 fn part1(input: Str) !u32 {
@@ -54,9 +54,35 @@ fn part1(input: Str) !u32 {
 }
 
 fn part2(input: Str) !u32 {
-    _ = input;
-    var answer: u32 = 0;
-    return answer;
+    var distance: u32 = 0;
+    var aim: i32 = 0;
+    var depth: i32 = 0;
+
+    // Let's use a quicker parsing method, directly stepping through chars.
+    var i: usize = 0;
+    while (i < input.len) : (i += 1) {
+        const direction: enum { Forward, Down, Up } = switch (input[i]) {
+            'f' => .Forward,
+            'd' => .Down,
+            'u' => .Up,
+            else => return error.UnexpectedChar,
+        };
+        const num = try readLineNum(input, &i);
+        switch (direction) {
+            .Forward => {
+                distance += num;
+                depth += aim * num;
+            },
+            .Down => {
+                aim += num;
+            },
+            .Up => {
+                aim -= num;
+            },
+        }
+    }
+
+    return @intCast(u32, depth) * distance;
 }
 
 pub fn main() !void {
@@ -75,4 +101,16 @@ test "2a" {
         \\forward 2
     ;
     try std.testing.expectEqual(@as(u32, 150), try part1(input));
+}
+
+test "2b" {
+    const input =
+        \\forward 5
+        \\down 5
+        \\forward 8
+        \\up 3
+        \\down 8
+        \\forward 2
+    ;
+    try std.testing.expectEqual(@as(u32, 900), try part2(input));
 }
