05bbb06bf7a477631002f923074bdb1af663ef23
Lewis Gaul <>
2021-12-06 01:08:01 +0000

    Move LineIter to util.zig

--- src/util.zig
+++ src/util.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
 const print = std.debug.print;
+const ParseIntError = std.fmt.ParseIntError;
 
-var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
-pub const gpa = &gpa_impl.allocator;
+const Str = []const u8;
 
 pub fn runPart(part: fn (Str) anyerror!u32, data: Str) !void {
     const start = std.time.nanoTimestamp();
@@ -25,3 +19,23 @@ pub fn runPart(part: fn (Str) anyerror!u32, data: Str) !void {
         },
     );
 }
+
+pub const LineIter = struct {
+    _iter: std.mem.SplitIterator(u8),
+
+    const Self = @This();
+
+    pub fn init(input: Str) Self {
+        return .{ ._iter = std.mem.split(u8, input, "\n") };
+    }
+
+    pub fn next(self: *Self) ParseIntError!?u32 {
+        if (self._iter.next()) |line| {
+            const trimmed = std.mem.trim(u8, line, " \t\r");
+            if (trimmed.len == 0) return try self.next();
+            return try std.fmt.parseUnsigned(u32, trimmed, 10);
+        } else {
+            return null;
+        }
+    }
+};
